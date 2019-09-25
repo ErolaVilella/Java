7abdb8974536dc47eda31b0defcbf61fabf67499
Erola <vilellaetona@gmail.com>
2019-09-25 21:02:39 +0200

    Ex.7

---
+++ Projectes/Exercici 1/ExercicisSenzills7.java
@@ -0,0 +1,12 @@
+public class ExercicisSenzills7{
+    public static void main(String[] args) {
+
+    double num;
+
+    num = Math.random()*1000+0;
+
+    System.out.println((int)num);
+
+
+    }
+}
